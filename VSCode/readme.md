cb02456f4ff9a97bcc260c590a616b4853b725cf
Earlcoming <earlcoming@163.com>
2022-04-11 21:38:17 +0800

    'vue3'

---
+++ VSCode/readme.md
@@ -0,0 +1,29 @@
+## vscode 配置
+
+> id:8e7d4334b04bb81cadd9c1903647042e
+
+### auto rename tag
+
+> 不需要安装此插件，可以达到同样的效果, `settings`搜索`Linked Editing`，勾上即可
+
+### 语言包
+
+> 安装好之后，按`ctrl+shift+p` 输入 `language` 选择`display language` 设置 `cn`
+
+### 格式化代码
+
+> prettier
+> `settings`里面搜索`editor:default format`选择 `prettier - code formatter`
+> 勾上下面保存时候自动格式化
+
+### JavaScript (ES6) snippets
+
+> 查询文档
+
+### Bracket Pair Colorizer 2
+
+> 括号颜色设置
+
+### 字体
+
+> fira code
--- Vue3/vue3-study/src/components/HelloWorld.vue
+++ Vue3/vue3-study/src/components/HelloWorld.vue
@@ -1,44 +1,44 @@
 <template>
   <div class="hello">
     <h1>setup和ref的基本使用</h1>
-    
-    <!-- <h3>{{count}}</h3> -->
-    <h2>姓名： {{form.name}}</h2>
-    <h2>年龄： {{form.age}}</h2>
-    <h2>媳妇： {{form.wife.name}} {{form.wife.age}} {{form.wife.cars[0]}}</h2>
-    <h2>职业： {{form.value}}</h2>
-    <h4></h4>
 
-    
+    <!-- <h3>{{count}}</h3> -->
+    <h2>姓名： {{ form.name }}</h2>
+    <h2>年龄： {{ form.age }}</h2>
+    <h2>
+      媳妇： {{ form.wife.name }} {{ form.wife.age }} {{ form.wife.cars[0] }}
+    </h2>
+    <h2>职业： {{ form.value }}</h2>
+    <h4>{{ form.sex }}</h4>
     <button @click="updateCount">按钮</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, reactive } from 'vue';
+import { defineComponent, ref, reactive } from "vue";
 export default defineComponent({
-  name: 'HelloWorld',
+  name: "HelloWorld",
   // setup执行时机，在beforeCreate之前执行(一次), 此时组件对象还没有创建
   mounted() {
-    console.log('mouted',this)
+    console.log("mouted", this);
   },
-   setup() {
-    const count = ref(1)
-    console.log(this)
+  setup() {
+    const count = ref(1);
+    console.log(this);
     // ref定义一个函数，作用：响应式数据(基本类型数据)
     const state = {
-      name: '小明',
+      name: "小明",
       age: 10,
       sex: 18,
-      value: '学生',
+      value: "学生",
       wife: {
-        name: '小甜甜',
+        name: "小甜甜",
         age: 18,
-        cars: ['宝马', '奔驰', '奥迪']
-      }
-    }
-    const form = reactive<any>(state)
-    
+        cars: ["宝马", "奔驰", "奥迪"],
+      },
+    };
+    const form = reactive<any>(state);
+
     const updateCount = () => {
       // console.log("数字+1", count)
       // count.value ++
@@ -49,19 +49,17 @@ export default defineComponent({
       // // state.gender = '男'
       // state.age = 1111
       // console.log(state, form)
-      form.age = 120
-      form.gender = 'test'
+      form.age = 120;
+      form.gender = "test";
       // delete state.sex
-      console.log(form, state)
-
-
-    }
+      console.log(form, state);
+    };
 
     return {
       count,
       updateCount,
-      form
-    }
+      form,
+    };
   },
 });
 </script>
