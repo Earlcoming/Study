9f20170d1bc07a6c05fa14ed6a349f080177595f
Earl <earlcoming@163.com>
2023-01-12 08:41:55 +0800

    'study'

--- nestjs/nest-demo1/src/main.ts
+++ nestjs/nest-demo1/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    // 开启Fastify
+    new FastifyAdapter(),
+    // 开启跨域, 开启日志(错误和警告)
+    { cors: true, logger: ['error', 'warn'] }
+  );
+
+  // 使用swagger
+  const options = new DocumentBuilder()
+    .setTitle('我的测试项目')
+    .setDescription('测试项目描述')
+    .setVersion('1.0')
+    .addTag('Earl')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000);
 }
 bootstrap();
+
+
---
+++ nestjs/nest-demo1/src/users/users.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+export type User = any
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[];
+  constructor() {
+    this.users = [
+      {
+        userId: 1,
+        username: 'john',
+        password: 'changeme',
+      },
+      {
+        userId: 2,
+        username: 'chris',
+        password: 'secret',
+      },
+      {
+        userId: 3,
+        username: 'maria',
+        password: 'guess',
+      },
+    ]
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  async findOne(id: number): Promise<User | undefined> {
+    return this.users.find(user => user.id === id)
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
